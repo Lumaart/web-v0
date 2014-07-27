4970aae1a81c7ac808f155a60b01109f6f740a32
alojzije <shljunak@gmail.com>
2014-07-27 22:59:44 +0200

    Add function that checks if an array contains an element

--- js/randomize_home_pics.js
+++ js/randomize_home_pics.js
@@ -2,15 +2,22 @@ function isMobile(){
     return 'ontouchstart' in document.documentElement
 }
 
+Array.prototype.contains = function (x) {
+    var len = this.length
+    for (var i = 0; i<len; i++) {
+        if (this[i] === x)  return true;
+    }
+    return false;
+}
+
 $(document).ready(function(){
     var pics = [];
     while(pics.length < 5){
         var rand = Math.ceil(Math.random()*8);
-        if (! (rand in pics))
+        if (!pics.contains(rand))
             pics.push(rand);
     }
-
     var suffix = isMobile() ? '_small.jpg' : '.jpg'
-    for (var i = 0; i<5; i++)
-        $('#homeslide_'+(i+1)).css('background-image', 'url(images/' + pics[i] + suffix + ')');
+    for (var i = 0; i<pics.length; i++)
+        $('#homeslide_'+ (i+1)).css('background-image', 'url(images/' + pics[i] + suffix + ')');
 });
